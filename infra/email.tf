4567c978baee985bac18a9066c6f749110eb1001
vivithebest <kt.kington@gmail.com>
2025-05-29 16:36:59 +0800

    [Teng Jin] #INFRA: add pipeline notification

--- infra/email.tf
+++ infra/email.tf
@@ -36,3 +36,49 @@ resource "aws_ses_domain_mail_from" "default" {
   domain           = aws_ses_domain_identity.default.domain
   mail_from_domain = "notification.${var.domain_name}"
 }
+
+# github action email iam
+resource "aws_iam_user" "github_actions_ses" {
+  name = "${var.project_name}-github-actions-ses"
+  tags = {
+    Project = var.project_name
+    Purpose = "Allow GitHub Actions to send SES emails"
+  }
+}
+
+resource "aws_iam_policy" "ses_send_policy" {
+  name_prefix = "${var.project_name}-ses-send-policy-"
+  description = "Allows sending emails via SES"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ses:SendEmail",
+          "ses:SendRawEmail"
+        ]
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_user_policy_attachment" "ses_send_attachment" {
+  user       = aws_iam_user.github_actions_ses.name
+  policy_arn = aws_iam_policy.ses_send_policy.arn
+}
+
+resource "aws_iam_access_key" "github_actions_ses" {
+  user = aws_iam_user.github_actions_ses.name
+}
+
+output "github_actions_ses_iam_access_key" {
+  value     = aws_iam_access_key.github_actions_ses.id
+  sensitive = true
+}
+
+output "github_actions_ses_iam_secret_key" {
+  value     = aws_iam_access_key.github_actions_ses.secret
+  sensitive = true
+}

d7cdc9b442d096a78f754fa23d6f08c8961cf2a6
vivithebest <kt.kington@gmail.com>
2025-05-30 13:32:28 +0800

    [Teng Jin] #INFRA: add app domain A record

--- infra/route53.tf
+++ infra/route53.tf
@@ -32,3 +32,27 @@ resource "aws_acm_certificate_validation" "main" {
   certificate_arn         = aws_acm_certificate.main.arn
   validation_record_fqdns = [for record in aws_route53_record.acm_validation : record.fqdn]
 }
+
+resource "aws_route53_record" "app_domain" {
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.main.dns_name
+    zone_id                = aws_lb.main.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "www_app_domain" {
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = "www.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.main.dns_name
+    zone_id                = aws_lb.main.zone_id
+    evaluate_target_health = true
+  }
+}
